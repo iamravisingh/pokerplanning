ea639fdc2621ce4d0e00e68fa26851b137784cf7
Ravi Singh <ravi.u.singh@here.com>
2023-07-29 09:12:18 +0530

    added create modal with material ui

--- apps/client/src/components/Home/home.tsx
+++ apps/client/src/components/Home/home.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { CreateRoom } from "./components"
 import "./style.css"
 
 export const Home = () => {
+  const [ openRoomModal, setRoomModal ] = useState(false);  
+
+  const handleCreateRoom = () => {
+    setRoomModal(true)
+  }
   return (
     <div>
       {/* <Header/> */}
@@ -9,7 +17,11 @@ export const Home = () => {
         <Box className="pokerHeadline">
             <h2>Planning Poker</h2>
             <span>Online Planning Poker for Agile Teams</span>
+            <Box mt={2} className="createRoom">   
+                <Button onClick={handleCreateRoom}>Create Room</Button>
+            </Box>
         </Box>
+        {openRoomModal && <CreateRoom open={openRoomModal} setRoomModal={setRoomModal}/>}
       </section>
       {/* <Footer/> */}
     </div>
