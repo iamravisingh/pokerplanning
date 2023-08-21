6a012bb55f485d9452870cf1ffa55191b9e07fce
Ravi Singh <ravi.u.singh@here.com>
2023-08-21 09:02:32 +0530

    updated the client socket handler

--- apps/client/src/common/hooks/index.ts
+++ apps/client/src/common/hooks/index.ts
@@ -1,1 +1,1 @@
-export { useSocketConnection, SocketInstance } from './useSocket';
+export { useSocketConnection } from './useSocket';
--- apps/client/src/common/hooks/useSocket.ts
+++ apps/client/src/common/hooks/useSocket.ts
@@ -2,32 +2,34 @@ import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { SERVER_URL } from '../../config';
 
-export type SocketInstance = Socket | null;
+export type SocketInstanceType = Socket | null;
 
 export const useSocketConnection = () => {
-  const [socketInstance, setSocketInstance] = useState<SocketInstance>(null);
+  const [socketInstance, setSocketInstance] =
+    useState<SocketInstanceType>(null);
+
   useEffect(() => {
-    let ignore = false;
-    if (!ignore) {
-      const socket = io(SERVER_URL, { autoConnect: false });
+    const socket = io(SERVER_URL, { autoConnect: false });
 
-      // Socket.IO event handlers
-      socket.on('connect', () => {
-        console.log('Connected to server');
-      });
+    const handleConnect = () => {
+      console.log('Connected to server');
+    };
 
-      socket.on('disconnect', () => {
-        console.log('Disconnected from server');
-      });
-      setSocketInstance(socket);
-    }
+    const handleDisconnect = () => {
+      console.log('Disconnected from server');
+    };
+    // Socket.IO event handlers
+    socket.on('connect', () => handleConnect);
+
+    socket.on('disconnect', handleDisconnect);
+    setSocketInstance(socket);
 
     return () => {
-      ignore = true;
-      if (socketInstance) {
-        socketInstance.disconnect();
-      }
+      socket.off('connect', handleConnect);
+      socket.off('disconnect', handleDisconnect);
+      socket.disconnect();
     };
-  }, [socketInstance]);
+  }, []);
+
   return { socketInstance };
 };
